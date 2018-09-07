aae64d9e18da6e9489489a99727d4cb3c14d81cc
Osajulian <you@example.com>
2018-09-07 11:38:12 -0400

    handling events

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -5,24 +5,27 @@ class Counter extends Component {
         count: 10,
         // tags: []
         tags: ['tag1', 'tag2', 'tag3', 'tag4']
-        
+
     }
 
-  
+
 
     render() {
-        
+
         return (
             <div>
-            {this.state.tags.length === 0 && <p>There are no tags!!!</p>}
-            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-               
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+
             </div>
         );
 
     }
 
- 
+
+
+    handleIncrement() {
+        console.log('Increment clicked');
+    }
 }
 
 export default Counter;
