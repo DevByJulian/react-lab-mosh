b363d6eba759142dda7117d182b7b3c0791e8fd5
Osajulian <you@example.com>
2018-09-10 15:01:45 -0400

    clean code

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -17,7 +17,7 @@ class Counter extends Component {
             <div>
             {/*// the below example is for demonstrating how props.children works.
             //Theis example easily have been achieved by using props. props.children are best used for complex objects */}
-            <h1>{this.props.children}</h1>
+            {this.props.children}
                 <button onClick={() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm m-2">Increment</button>
                 <button onClick={this.handleDecrement} className="btn btn-primary btn-sm">Decrement</button>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
